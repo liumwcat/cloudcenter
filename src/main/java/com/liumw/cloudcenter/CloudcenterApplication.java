9ffec3f4abd591a0dfe808eea56f1f67c7604213
liumw <liumingwei1012@163.com>
2020-01-13 15:18:01 +0800

    修改配置

---
+++ src/main/java/com/liumw/cloudcenter/CloudcenterApplication.java
@@ -0,0 +1,13 @@
+package com.liumw.cloudcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liumw/cloudcenter/CloudcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liumw.cloudcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
